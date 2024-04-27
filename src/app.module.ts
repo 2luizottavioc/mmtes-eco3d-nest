76652ba96fbd87e64a273c62bd918a7917bcb9e1
Luiz Carvalho <luizottavio49@gmail.com>
2024-04-26 21:16:22 -0300

    user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule],
+  imports: [UsersModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
