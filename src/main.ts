94ac3ab5d24e283bd23a5bb6439bf82b7dad09fe
Luiz Carvalho <luizottavio49@gmail.com>
2024-05-17 18:42:15 -0300

    enable cors + change listen port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,7 +13,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 
 bootstrap();
